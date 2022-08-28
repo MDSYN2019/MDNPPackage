330a86b4ed850cccb3e9a9f15fa6ff617f1e9a8d
Sang <sangyoung123@googlemail.com>
2022-08-28 22:17:20 +0100

    removed annoying files

--- MDNPPackage/vis/NP_Vis.py
+++ MDNPPackage/vis/NP_Vis.py
@@ -1,16 +1,22 @@
-import streamlit as st
 import pandas as pd
 import numpy as np
-
-
 import sys
+
 # plotly functionalities 
 import plotly.graph_objs as go
 import plotly.express as px
 from plotly.subplots import make_subplots
 
-def dashboard_NP():
+# streamlit dashboard
+import streamlit as st
+
+
+def dashboard_NP(dataframe: pd.DataFrame):
     """
+    first function for dashboard
     """
-    pass
+    fig = px.scatter_3d(dataframe, x='X', y='Y', z='Z', color='name')
+    return fig 
+    
+
 
