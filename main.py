0d0b779287b573ab47375acd8437a1311677092d
Sang <sangyoung123@googlemail.com>
2022-09-02 14:28:44 +0100

    added samples

--- main.py
+++ main.py
@@ -10,9 +10,22 @@ ligand_2_path = gro_path + "/" + "2NITL.gro" # Second ligand
 itp_1_path = '/home/sang/Desktop/git/Martini3-small-molecules/models/itps/cog-mono/1MIMI_cog.itp'
 itp_2_path = '/home/sang/Desktop/git/Martini3-small-molecules/models/itps/cog-mono/2NITL_cog.itp'
 
-CoreGen = CentralCoreGenerator(10, 50, [ligand_1_path, ligand_2_path], ['N1', 'N1'] , ['R3', 'R4'],  itp_1_path, itp_2_path, option = 'Janus')
-return_streamlit_NP(CoreGen.return_ordered_coordinates()) # show as streamlit dashboard 
-CoreGen.generate_coordinates('NP')
-CoreGen.generate_itp('NP')
+
+# --
+
+ligand_3_path = gro_path + "/" + "ANIL.gro" # First ligand 
+ligand_4_path = gro_path + "/" + "4NIAN.gro" # Second ligand 
+itp_3_path = '/home/sang/Desktop/git/Martini3-small-molecules/models/itps/cog-mono/ANIL_cog.itp'
+itp_4_path = '/home/sang/Desktop/git/Martini3-small-molecules/models/itps/cog-mono/4NIAN_cog.itp'
+
+CoreGen_I = CentralCoreGenerator(10, 50, [ligand_1_path, ligand_2_path], ['N1', 'N1'] , ['R3', 'R4'],  itp_1_path, itp_2_path, option = 'Striped')
+##return_streamlit_NP(CoreGen_I.return_ordered_coordinates()) # show as streamlit dashboard 
+CoreGen_I.generate_coordinates('NP_MIMI_NITL')
+CoreGen_I.generate_itp('NP_MIMI_NITL')
+
+CoreGen_II = CentralCoreGenerator(10, 50, [ligand_3_path, ligand_4_path], ['N1', 'N1'] , ['R3', 'O4'],  itp_3_path, itp_4_path, option = 'Striped')
+##return_streamlit_NP(CoreGen_II.return_ordered_coordinates()) # show as streamlit dashboard 
+CoreGen_II.generate_coordinates('NP_ANIL_4NIAN')
+CoreGen_II.generate_itp('NP_ANIL_4NIAN')
 
 
