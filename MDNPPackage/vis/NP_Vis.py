07a54d371a9460ff3cccb9250125597e2101a7c0
Sang <sangyoung123@googlemail.com>
2022-09-02 13:44:05 +0100

    removed more unneeded lines

--- MDNPPackage/vis/NP_Vis.py
+++ MDNPPackage/vis/NP_Vis.py
@@ -9,18 +9,7 @@ import plotly.graph_objs as go
 import plotly.express as px
 from plotly.subplots import make_subplots
 import core.NP_Core as core 
-#from core import CentralCoreGenerator 
-# streamlit dashboard
 import streamlit as st
-
-gro_path = "/home/sang/Desktop/git/Martini3-small-molecules/models/gros" 
-ligand_1_path = gro_path + "/" + "1MIMI.gro" # First ligand 
-ligand_2_path = gro_path + "/" + "2NITL.gro" # Second ligand 
-itp_1_path = '/home/sang/Desktop/git/Martini3-small-molecules/models/itps/cog-mono/1MIMI_cog.itp'
-itp_2_path = '/home/sang/Desktop/git/Martini3-small-molecules/models/itps/cog-mono/2NITL_cog.itp'
-
-CoreGen = core.CentralCoreGenerator(10, 50, [ligand_1_path, ligand_2_path], ['N1', 'N1'] , ['R3', 'R4'],  itp_1_path, itp_2_path, option = 'Janus')
-DF = CoreGen.return_ordered_coordinates()
 
 def dashboard_NP(dataframe: pd.DataFrame):
     """
